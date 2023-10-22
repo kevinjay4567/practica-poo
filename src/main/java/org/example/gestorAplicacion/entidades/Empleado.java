e16ac3edd637821f7058c1d816d28e19295bdf70
kevinjay4567 <kjayh@unal.edu.co>
2023-10-21 22:01:59 -0500

    Agregado de la clase Persona

---
+++ src/main/java/org/example/gestorAplicacion/entidades/Empleado.java
@@ -0,0 +1,49 @@
+package org.example.gestorAplicacion.entidades;
+
+public class Empleado extends Persona{
+    private String IDEmpleado;
+    private String rol;
+    private Double sueldo;
+
+    public Empleado(String nombre, Integer edad, Character sexo, String tipo_documento, Long num_documento, String telefono, String direccion, String correo, Integer calificacion, String IDEmpleado, String rol, Double sueldo) {
+        super(nombre, edad, sexo, tipo_documento, num_documento, telefono, direccion, correo);
+        this.IDEmpleado = IDEmpleado;
+        this.rol = rol;
+        this.sueldo = sueldo;
+    }
+
+    public String getIDEmpleado() {
+        return IDEmpleado;
+    }
+
+    public void setIDEmpleado(String IDEmpleado) {
+        this.IDEmpleado = IDEmpleado;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String personaRol() {
+        return "Hola";
+    }
+    public void ascender() {
+
+    }
+    @Override
+    public String toString() {
+        return null;
+    }
+}
--- src/main/java/org/example/gestorAplicacion/entidades/Persona.java
+++ src/main/java/org/example/gestorAplicacion/entidades/Persona.java
@@ -1,4 +1,94 @@
 package org.example.gestorAplicacion.entidades;
 
-public class Persona {
+public abstract class Persona {
+    private String nombre;
+    private Integer edad;
+    private Character sexo;
+    private String tipo_documento;
+    private Long num_documento;
+    private String telefono;
+    private String direccion;
+    private String correo;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, Integer edad, Character sexo, String tipo_documento, Long num_documento, String telefono, String direccion, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.tipo_documento = tipo_documento;
+        this.num_documento = num_documento;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTipo_documento() {
+        return tipo_documento;
+    }
+
+    public void setTipo_documento(String tipo_documento) {
+        this.tipo_documento = tipo_documento;
+    }
+
+    public Long getNum_documento() {
+        return num_documento;
+    }
+
+    public void setNum_documento(Long num_documento) {
+        this.num_documento = num_documento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public abstract String toString();
+    public abstract void ascender();
+    public abstract String personaRol();
 }
